807133c46b793b9fa5792c37f4609bc04ee46e41
Kurt Thomas <kurt.thomas34@gmail.com>
2016-10-18 17:36:49 +0000

    completed A5 added remove_entry test

--- models/address_book.rb
+++ models/address_book.rb
@@ -23,7 +23,7 @@ class AddressBook
     def remove_entry(name, phone_number, email)
         index = 0
         entries.each do |entry|
-            if name == entry.name
+            if name == entry.name && phone_number == entry. phone_number && email == entry.email
                 entries.delete(entry)
                 break
             else
